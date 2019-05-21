15932a4ef3f94ca77754b8fbdd46d2defeb146e0
Specialist001 <darking-uz@yandex.ru>
2019-05-21 14:34:18 +0500

    added .htaccess

--- backend/config/main.php
+++ backend/config/main.php
@@ -37,14 +37,15 @@ return [
         'errorHandler' => [
             'errorAction' => 'site/error',
         ],
-        /*
+
         'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
+                'dashboard' => 'site/index',
             ],
         ],
-        */
+
     ],
     'params' => $params,
 ];
