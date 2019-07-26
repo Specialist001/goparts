cf9eada04daaa32248b72cb69303e47fa1e9cedc
Specialist001 <darking-uz@yandex.ru>
2019-07-26 15:37:21 +0500

    last commit

--- api/controllers/ProfileController.php
+++ api/controllers/ProfileController.php
@@ -27,8 +27,8 @@ class ProfileController extends \yii\web\Controller
         return [
             'authenticator' => [
                 'class' => HttpBasicAuth::className(),
-                'auth' => function ($username, $password) {
-                    $user = User::findByUsername($username);
+                'auth' => function ($email, $password) {
+                    $user = User::findByEmail($email);
                     if (!$user) return null;
                     $check = $user->validatePassword($password);
                     return $check ? $user: null;
